a8b8907e72f58cc28222078eb14ad5327b5e1ef4
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-12-01 17:44:51 -0800

    Updated

--- dispatchers.py
+++ dispatchers.py
@@ -11,33 +11,35 @@ from collections import OrderedDict as ODict
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['clskey_singledispatcher', 'clstype_singledispatcher', 'type_singledispatcher', 'key_singledispatcher', 'keyword_singledispatcher']
+__all__ = ['key_singledispatcher', 'clskey_singledispatcher', 'type_singledispatcher', 'clstype_singledispatcher', 'keyword_singledispatcher']
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
 
-def clskey_singledispatcher(key): 
-    def decorator(mainfunc):
-        _registry = ODict()
+def clskey_singledispatcher(mainfunc):
+    _registry = ODict()
     
-        def wrapper(self, *args, **kwargs): return _registry.get(kwargs[key], mainfunc)(self, *args, **kwargs)
-        def update(regfunc, *values): _registry.update({value:regfunc for value in values}) 
-        def registry(): return _registry
+    def update(regfunc, *keys): _registry.update({key:regfunc for key in keys})
+    def registry(): return _registry
     
-        def register(*values): 
-            def register_decorator(regfunc): 
-                update(regfunc, *values) 
-                def register_wrapper(*args, **kwargs): 
-                    return regfunc(*args, **kwargs) 
-                return register_wrapper 
-            return register_decorator 
-            
-        wrapper.register = register
-        wrapper.registry = registry
-        update_wrapper(wrapper, mainfunc)
-        return wrapper
-    return decorator
-    
+    def register(*keys): 
+        def register_decorator(regfunc): 
+            update(regfunc, *keys) 
+            def register_wrapper(*args, **kwargs): 
+                return regfunc(*args, **kwargs) 
+            return register_wrapper 
+        return register_decorator 
+
+    def wrapper(self, key, *args, **kwargs): 
+        func = _registry.get(key, mainfunc)
+        if key in _registry.keys(): return func(self, *args, **kwargs)         
+        else: return func(self, key, *args, **kwargs)
+
+    wrapper.register = register 
+    wrapper.registry = registry
+    update_wrapper(wrapper, mainfunc)
+    return wrapper
+
 
 def key_singledispatcher(mainfunc):
     _registry = ODict()
