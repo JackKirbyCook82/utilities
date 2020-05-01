76632e53724956106e7528c245f338c810554cd1
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-05-01 16:15:26 -0700

    Updated'

--- concepts.py
+++ concepts.py
@@ -26,11 +26,14 @@ def concept(name, fields):
     def __new__(cls, **kwargs): return super().__new__(cls, *[kwargs.get(field, None) for field in cls._fields])
     def __getitem__(self, key): return self._asdict().items()[key]  
     def __hash__(self): return hash((self.__class__.__name__, *[hash((concept, hash(getattr(self, field)),)) for field in self._fields]))
+    
+    @property
+    def fields(self): return self._fields
     def todict(self): return self._asdict()   
     
     name = uppercase(name)
     base = ntuple(uppercase(name), ' '.join([field.lower() for field in _aslist(fields)]))    
-    attrs = {'todict':todict, '__new__':__new__, '__getitem__':__getitem__, '__hash__':__hash__}
+    attrs = {'todict':todict, 'fields':fields, '__new__':__new__, '__getitem__':__getitem__, '__hash__':__hash__}
     return type(name, (base,), attrs)
 
 
