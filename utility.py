149dab012a67395155db00aed506ab9654a07349
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-05-18 16:22:26 -0700

    Updated

--- utility.py
+++ utility.py
@@ -44,7 +44,7 @@ class UtilityIndex(ABC):
         return hash((self.__class__.__name__, self.functionname, self.functiontype, self.amplitude, coefficients,))
         
     def __len__(self): return len(self.parameters)
-    def __init__(self, amplitude=1, tolerances={}): 
+    def __init__(self, *args, amplitude=1, tolerances={}, **kwargs): 
         assert isinstance(tolerances, dict)
         self.amplitude = amplitude
         self.tolerances = np.array([tolerances.get(parm, 1) for parm in self.parameters])    
@@ -58,20 +58,20 @@ class UtilityIndex(ABC):
         return y
        
     @classmethod
-    def register(cls, functiontype, parameter_weights):
+    def register(cls, functionname, functiontype, parameter_weights):
         if cls != UtilityIndex: raise NotImplementedError('{}.{}()'.format(cls.__name__, 'create'))
         assert functiontype in INDEXFUNCTIONS.keys()
         assert isinstance(parameter_weights, dict)
         parameters = list(parameter_weights.keys())
         weights = _normalize(list(parameter_weights.values()))
-        attrs = dict(parameters=parameters, weights=weights, functiontype=functiontype, function=INDEXFUNCTIONS[functiontype])
+        attrs = dict(parameters=parameters, weights=weights, functionname=functionname, functiontype=functiontype, function=INDEXFUNCTIONS[functiontype])
         def wrapper(subclass): return type(subclass.__name__, (subclass, cls), attrs)
         return wrapper
     
     @classmethod
-    def subclasses(cls): return {subclass.functiontype:subclass for subclass in cls.__subclasses__()}     
+    def subclasses(cls): return {subclass.functionname:subclass for subclass in cls.__subclasses__()}     
     @classmethod
-    def create(cls, functiontype, *args, **kwargs): return cls.subclasses()[functiontype](*args, **kwargs)
+    def create(cls, functionname, *args, **kwargs): return cls.subclasses()[functionname](*args, **kwargs)
 
 
 class UtilityFunction(ABC):
@@ -88,9 +88,9 @@ class UtilityFunction(ABC):
         return hash((self.__class__.__name__, self.functiontype, indexes,))
     
     def __len__(self): return len(self.parameters)
-    def __init__(self, parameters, *args, **kwargs):
-        assert isinstance(parameters, dict)
-        self.parameters, self.indexes = parameters.keys(), parameters.values()
+    def __init__(self, *args, indexes, **kwargs):
+        assert isinstance(indexes, dict)
+        self.parameters, self.indexes = indexes.keys(), indexes.values()
             
     def __call__(self, *args, **kwargs): 
         x = np.array([index(*args, **kwargs) for parameter, index in zip(self.parameters, self.indexes)])
@@ -98,7 +98,7 @@ class UtilityFunction(ABC):
         return y   
 
     @classmethod
-    def register(cls, functiontype):
+    def register(cls, functionname, functiontype):
         if cls != UtilityFunction: raise NotImplementedError('{}.{}()'.format(cls.__name__, 'create'))
         assert functiontype in UTILITYFUNCTIONS.keys()
         attrs = dict(functiontype=functiontype, function=UTILITYFUNCTIONS[functiontype])
@@ -106,12 +106,12 @@ class UtilityFunction(ABC):
         return wrapper   
     
     @classmethod
-    def subclasses(cls): return {subclass.functiontype:subclass for subclass in cls.__subclasses__()}     
+    def subclasses(cls): return {subclass.functionname:subclass for subclass in cls.__subclasses__()}     
     @classmethod
-    def create(cls, functiontype, *args, **kwargs): return cls.subclasses()[functiontype](*args, **kwargs)
+    def create(cls, functionname, *args, **kwargs): return cls.subclasses()[functionname](*args, **kwargs)
 
 
-@UtilityFunction.register('cobbdouglas')
+@UtilityFunction.register('cobbdouglas', 'cobbdouglas')
 class CobbDouglas_UtilityFunction(UtilityFunction):
     @property
     def coefficients(self): return self.amplitude, self.subsistences, self.diminishrate, self.weights    
