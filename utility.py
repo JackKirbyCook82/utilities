6859cbd625072db7ff4ac52d9ea65408baf22677
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-06-11 16:02:54 -0700

    Updated

--- utility.py
+++ utility.py
@@ -7,95 +7,138 @@ Created on Sun Feb 23 2020
 """
 
 from abc import ABC, abstractmethod
+from collections import OrderedDict as ODict
 import numpy as np
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['UtilityIndex', 'CobbDouglas_UtilityFunction']
+__all__ = ['UtilityIndex', 'UtilityFunction']
 __copyright__ = "Copyright 2020, Jack Kirby Cook"
 __license__ = ""
 
-
-INDEXFUNCTIONS = {
-    'inverted': lambda t, w, x: np.sum(np.divide(np.divide(w, t), x)),
-    'tangent': lambda t, w, x: np.sum(np.multiply(np.divide(w, t), np.tan(x * np.pi/2))),
-    'rtangent': lambda t, w, x: np.sum(np.multiply(np.divide(w, t), np.tan((1 - x) * np.pi/2))),
-    'logarithm': lambda t, w, x: np.sum(np.multiply(np.divide(w, t), np.log(x + 1)))}
 
 _aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else list(items)
 _normalize = lambda items: np.array(items) / np.sum(np.array(items))
 
 
+UTILITYFUNCTIONS = {
+    'cobbdouglas': lambda a, d, s, w, x: (np.prod(np.power(np.subtract(x, s), w)) ** d) * a,
+    'ces': lambda a, d, s, w, x, p: (np.sum(np.multiply((np.subtract(x, s) ** p), w)) ** (d/p)) * a}
+INDEXFUNCTIONS = {
+    'inverted': lambda a, t, w, x: np.sum(np.divide(np.divide(w, t), x)),
+    'tangent': lambda a, t, w, x: np.sum(np.multiply(np.divide(w, t), np.tan(x * np.pi/2))),
+    'rtangent': lambda a, t, w, x: np.sum(np.multiply(np.divide(w, t), np.tan((1 - x) * np.pi/2))),
+    'logarithm': lambda a, t, w, x: np.sum(np.multiply(np.divide(w, t), np.log(x + 1)))}
+
+
 class UtilityIndex(ABC): 
     @abstractmethod
     def execute(self, *args, **kwargs): pass
+    @classmethod    
+    @abstractmethod
+    def create(self, *args, **kwargs): pass
+    @classmethod    
+    @abstractmethod
+    def function(self, *args, **kwargs): pass
 
     @property
-    def key(self): return (self.functionname, self.functiontype, self.amplitude, *[item for item in self.items()],)
-    def items(self): return [(parameter, weight, tolerance) for parameter, weight, tolerance in zip(self.parameters, self.weights, self.tolerances)]  
+    def key(self): return hash((self.functionname, self.functiontype, self.amplitude, *[item for item in self.items()],))
+    def items(self): return [(parameter, self.tolerances[parameter], self.weight[parameter]) for parameter in self.parameters]  
     
     def __repr__(self): 
         string = '{}(functionname={}, functiontype={}, amplitude={}, tolerances={}, weights={})' 
-        tolerancesDict = {parameter:tolerance for parameter, tolerance in zip(self.parameters, self.tolerances)}
-        weightsDict = {parameter:weight for parameter, weight in zip(self.parameters, self.weights)}
-        return string.format(self.__class__.__name__, self.functionname, self.functiontype, self.amplitude, tolerancesDict, weightsDict)
+        return string.format(self.__class__.__name__, self.functionname, self.functiontype, self.amplitude, self.tolerances, self.weights)
         
     def __len__(self): return len(self.parameters)
-    def __init__(self, *args, amplitude=1, tolerances={}, **kwargs): 
-        assert isinstance(tolerances, dict)
+    def __init__(self, *args, amplitude=1, tolerances={}, weights={}, **kwargs): 
+        assert isinstance(tolerances, dict) and isinstance(weights, dict)
         self.amplitude = amplitude
-        self.tolerances = np.array([tolerances.get(parm, 1) for parm in self.parameters])    
+        self.tolerances = {tolerances.get(parm, 1) for parm in self.parameters}
+        self.weights = {weights.get(parm, 1) for parm in self.parameters}
  
     def __call__(self, *args, **kwargs): 
-        parameters = self.execute(*args, **kwargs)
-        assert isinstance(parameters, dict)
-        assert all([parameter in parameters.keys() for parameter in self.parameters])
-        x = np.array([parameters[parameter] for parameter in self.parameters])
-        y = self.amplitude * self.function(self.tolerances, self.weights, x)
-        return y
- 
+        values = self.execute(*args, **kwargs)
+        assert isinstance(values, dict)
+        assert all([parameter in values.keys() for parameter in self.parameters])
+        t = np.array([self.tolerances[parameter] for parameter in self.parameters])
+        w = _normalize(np.array([self.weights[parameter] for parameter in self.parameters]))
+        x = np.array([values[parameter] for parameter in self.parameters])
+        return self.function(self.amplitude, t, w, x)  
+    
+    __subclasses = {}          
     @classmethod
-    def create(cls, functionname, functiontype, parameter_weights):
-        if cls != UtilityIndex: raise NotImplementedError('{}.{}()'.format(cls.__name__, 'create'))
+    def subclasses(cls): return cls.__subclasses
+    @classmethod
+    def getsubclass(cls, functionname): return cls.__subclasses[functionname.lower()]   
+    def __class_getitem__(cls, functionname): return cls.getsubclass(functionname)
+    
+    @classmethod
+    def register(cls, functionname, functiontype, *args, parameters, **kargs):
+        if cls == UtilityIndex: raise NotImplementedError('{}.{}()'.format(cls.__name__, 'register'))      
+        assert isinstance(parameters, (tuple, list))
         assert functiontype in INDEXFUNCTIONS.keys()
-        assert isinstance(parameter_weights, dict)
-        parameters = list(parameter_weights.keys())
-        weights = _normalize(list(parameter_weights.values()))
-        attrs = dict(parameters=parameters, weights=weights, functionname=functionname, functiontype=functiontype, function=INDEXFUNCTIONS[functiontype])
-        def wrapper(subclass): return type(subclass.__name__, (subclass, cls), attrs)
+        attrs = dict(functionname=functionname, functiontype=functiontype, function=INDEXFUNCTIONS[functiontype], parameters=parameters)
+        def wrapper(subclass): 
+            newsubclass = type(subclass.__name__, (subclass, cls), attrs)
+            cls.__subclasses[functionname] = newsubclass
+            return newsubclass
+        return wrapper
+  
+    
+class UtilityFunction(ABC): 
+    @classmethod    
+    @abstractmethod
+    def create(self, *args, **kwargs): pass
+    @classmethod    
+    @abstractmethod
+    def function(self, *args, **kwargs): pass
+
+    @property
+    def key(self): return hash((self.functionname, self.functiontype, self.amplitude, *[(key, value) for key, value in self.coefficents.items()], *[item for item in self.items()],))
+    def items(self): return [(parameter, self.subsistences[parameter], self.weight[parameter]) for parameter in self.parameters]   
+    
+    def __repr__(self): 
+        content = dict(functionname=self.functionname, functiontype=self.functiontype, amplitude=self.amplitude, subsistences=self.subsistences, weights=self.weights)
+        content.update(self.coefficents)
+        return '{}({})'.format(self.__class__.__name__, ', '.join(['='.join([key, value]) for key, value in content.items()])) 
+        
+    def __len__(self): return len(self.parameters)
+    def __init__(self, *args, amplitude=1, diminishrate=1, subsistences={}, weights={}, indexes={}, **kwargs): 
+        assert isinstance(subsistences, dict) and isinstance(weights, dict)
+        self.__amplitude, self.__diminishrate = amplitude, diminishrate
+        self.__coefficents = ODict([(coefficent, kwargs[coefficent]) for coefficent in self.coefficents])
+        self.__subsistences = {subsistences.get(parm, 1) for parm in self.parameters}
+        self.__weights = {weights.get(parm, 1) for parm in self.parameters}
+        self.__indexes = {indexes[parm] for parm in self.parameters}
+
+    def __call__(self, *args, **kwargs): 
+        c = list(self.__coefficents.values())
+        s = np.array([self.__subsistences[parameter] for parameter in self.parameters])
+        w = _normalize(np.array([self.__weights[parameter] for parameter in self.parameters]))
+        x = np.array([self.__indexes[parameter](*args, **kwargs) for parameter in self.parameters])
+        return self.function(self.__amplitude, self.__diminishrate, s, w, x, *c)
+
+    __subclasses = {}          
+    @classmethod
+    def subclasses(cls): return cls.__subclasses
+    @classmethod
+    def getsubclass(cls, functionname): return cls.__subclasses[functionname.lower()]   
+    def __class_getitem__(cls, functionname): return cls.getsubclass(functionname)
+    
+    @classmethod
+    def register(cls, functionname, functiontype, *args, parameters, coefficents, **kwargs):
+        if cls == UtilityFunction: raise NotImplementedError('{}.{}()'.format(cls.__name__, 'register'))      
+        assert isinstance(parameters, (tuple, list)) and isinstance(coefficents, (tuple, list))
+        assert functiontype in INDEXFUNCTIONS.keys()       
+        attrs = dict(functionname=functionname, functiontype=functiontype, function=UTILITYFUNCTIONS[functiontype], parameters=parameters, coefficents=coefficents)
+        def wrapper(subclass): 
+            newsubclass = type(subclass.__name__, (subclass, cls), attrs)
+            cls.__subclasses[functionname] = newsubclass
+            return newsubclass
         return wrapper
     
 
-class CobbDouglas_UtilityFunction(object):
-    @property
-    def key(self): return (self.__class__.__name__, self.amplitude, self.diminishrate, *[(*item[:-1], item[-1].key) for item in self.items()],)
-    def items(self): return [(parameter, subsistence, weight, index) for parameter, subsistence, weight, index in zip(self.parameters, self.subsistences, self.weights, self.indexes,)]  
-    def function(self, a, b, c, w, x): return a * np.power(np.prod(np.power(np.subtract(x, b), w)), c)
-    
-    def __repr__(self): 
-        string = '{}(amplitude={}, diminishrate={}, subsistences={}, weights={}, indexes={})'
-        subsistencesDict = {parameter:subsistence for parameter, subsistence in zip(self.parameters, self.subsistences)}
-        weightsDict = {parameter:weight for parameter, weight in zip(self.parameters, self.weights)}
-        indexesDict = {parameter:repr(index) for parameter, index in zip(self.parameters, self.indexes)}
-        return string.format(self.__class__.__name__, self.functionname, self.functiontype, self.amplitude, self.diminishrate, subsistencesDict, weightsDict, indexesDict)
-    
-    def __len__(self): return len(self.parameters)
-    def __init__(self, *args, amplitude=1, diminishrate=1, subsistences={}, weights={}, indexes, **kwargs):
-        assert all([isinstance(items, dict) for items in (subsistences, weights, indexes)])
-        self.amplitude, self.diminishrate = amplitude, diminishrate
-        self.parameters, self.indexes = indexes.keys(), indexes.values()
-        self.subsistences = np.array([subsistences.get(parm, 0) for parm in self.parameters])
-        self.weights = np.array([weights.get(parm, 0) for parm in self.parameters])        
-        if np.all(self.weights == 0): self.weights = np.ones(self.weights.shape)
-        else: self.weights = _normalize(self.weights) 
 
-    def __call__(self, *args, **kwargs): 
-        x = np.array([index(*args, **kwargs) for parameter, index in zip(self.parameters, self.indexes)])
-        y = self.function(self.amplitude, self.subsistences, self.diminishrate, self.weights, x)
-        return y  
-
-
-    
 
 
 
