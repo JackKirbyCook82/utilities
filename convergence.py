ac1ee8da46b5fc56763571070758092c817e66fd
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-07-21 17:28:52 -0700

    Updated

--- convergence.py
+++ convergence.py
@@ -10,11 +10,9 @@ import numpy as np
 import pandas as pd
 from abc import ABC, abstractmethod
 
-from utilities.dispatchers import clskey_singledispatcher as keydispatcher
-
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['History', 'DurationDampener', 'OscillationDampener', 'ErrorConverger', 'ConvergenceError', 'OscillationConverger']
+__all__ = ['History', 'DurationDampener', 'ErrorConverger', 'ConvergenceError', 'OscillationConverger']
 __copyright__ = "Copyright 2020, Jack Kirby Cook"
 __license__ = ""
 
@@ -24,18 +22,18 @@ _divide = lambda x, y: x / y
 _window = lambda x, n, i: x[i:i+n]
 _void = lambda n: np.ones(n-1) * np.NaN
 _pad = lambda x, n, f: np.concatenate([_void(n), f(x, n)])
-_delta = lambda x: x[1:] - x[:-1]
-_direction = lambda x: _delta(x) / np.abs(_delta(x))
-_trend = lambda x: np.sum(_direction(x)) / (len(x) - 1) 
 _error = lambda x, rtol, atol: np.allclose(x, np.zeros(x.shape), rtol=rtol, atol=atol) 
-_sma = lambda x, n: np.convolve(x, np.ones(n)/n, 'valid')
-_mmax = lambda x, n: np.array([np.amax(_window(x, n, i)) for i in np.arange(len(x)-n+1)])
-_mmin = lambda x, n: np.array([np.amin(_window(x, n, i)) for i in np.arange(len(x)-n+1)])
-_mtrend = lambda x, n: np.array([_trend(_window(x, n, i)) for i in np.arange(len(x)-n+1)])
-_ema = lambda x, n, s: (x[-1] * (s / (1 + n))) + (_ema(x[:-1], n, s) if len(x) > 1 else 0) * (1 - (s / (1 + n)))
 _avgsqerr = lambda x: np.square(x).mean() ** 0.5
 _maxsqerr = lambda x: np.max(np.square(x)) ** 0.5
 _minsqerr = lambda x: np.min(np.square(x)) ** 0.5
+_delta = lambda x: x[1:] - x[:-1]
+_drt = lambda x: _delta(x) / np.abs(_delta(x))
+#_mdelta = lambda x, n: np.convolve(_delta(x), np.ones(n), 'valid')
+#_mdrt = lambda x, n: np.convolve(_drt(x), np.ones(n), 'valid')
+_sma = lambda x, n: np.convolve(x, np.ones(n)/n, 'valid')
+_mmax = lambda x, n: np.array([np.amax(_window(x, n, i)) for i in np.arange(len(x)-n+1)])
+_mmin = lambda x, n: np.array([np.amin(_window(x, n, i)) for i in np.arange(len(x)-n+1)])
+_ema = lambda x, n, s: (x[-1] * (s / (1 + n))) + (_ema(x[:-1], n, s) if len(x) > 1 else 0) * (1 - (s / (1 + n)))
 
 
 class Dampener(ABC):
@@ -53,20 +51,7 @@ class DurationDampener(Dampener):
         factors = np.floor((np.ones(data.shape[0]) * data.shape[-1]) / self.__period)
         sizes = np.ones(data.shape[0]) * self.__size
         return np.power(1 - sizes, factors)
-
-class OscillationDampener(Dampener): 
-    def __init__(self, *args, period, size, threshold, smoothing=2, **kwargs):
-        assert isinstance(period, int) and period > 0
-        assert isinstance(size, float) and 0 > size < 1
-        assert isinstance(threshold, float) and 0 <= threshold <= 1
-        self.__period, self.__size, self.__threshold, self.__smoothing = period, size, threshold, smoothing
-        
-    def execute(self, data): 
-        trends = np.floor(np.abs(np.apply_along_axis(_mtrend, 1, data, self.__period)) / self.__threshold)
-        oscillates = (np.maximum(trends, 1) - 1) * -1
-        factors = np.apply_along_axis(_ema, 1, oscillates, self.__period, self.__smoothing)
-        return np.apply_along_axis(lambda x: 1 - (1 - self.__size) * x, 1, factors)        
-
+    
 
 class ConvergenceError(Exception): pass
 class Converger(ABC): 
@@ -74,15 +59,6 @@ class Converger(ABC):
     def converged(self): pass
     @abstractmethod
     def limit(self): pass
-    
-    @keydispatcher
-    def error(self, how): raise KeyError(how)
-    @error.register('average', 'avg')
-    def error_average(self): return _avgsqerr(self.errors) if self.active else None
-    @error.register('maximum', 'max')
-    def error_maximum(self): return _maxsqerr(self.errors) if self.active else None
-    @error.register('minimum', 'min')
-    def error_minimum(self): return _minsqerr(self.errors) if self.active else None
                 
     def __bool__(self): return self.converged() if self.active else False
     def __len__(self): return self.values.shape[-1] if self.active else 0
@@ -102,10 +78,20 @@ class ErrorConverger(Converger):
         if not self.active: return False
         else: return _error(self.errors, self.__rtol, self.__atol)
 
-#class OscillationConverger(Converger):
-#   def __init__(self, *args, **kwargs): pass
-#    def limit(self): pass
-#    def converged(self): pass    
+class OscillationConverger(Converger):
+    def __init__(self, *args, period, tolerance, **kwargs): self.__period, self.__tolerance = period, tolerance
+    def limit(self): return np.average(np.apply_along_axis(_sma, 1, self.values[:, -self.__period:], self.__period), axis=1)
+    def converged(self): 
+        if not self.active or len(self) <= self.__period: return False
+        return all([self.__bounded(), self.__oscillating(), not self.__trending()])  
+
+    def __bounded(self):
+        xmax = np.apply_along_axis(np.max, 1, self.values[:, -self.__period:])
+        xmin = np.apply_along_axis(np.min, 1, self.values[:, -self.__period:])
+        return np.all(np.maximum((xmax - xmin) - self.__tolerance, 0) == 0)
+
+#    def __trending(self): pass   
+#    def __oscillating(self): pass
 
     
 class History(object):
